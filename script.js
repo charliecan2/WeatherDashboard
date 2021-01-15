0ceb0491df60d457e6158052bf09f8941b3aa648
charliecan2 <carloscq920@gmail.com>
2021-01-14 23:20:32 -0600

    Allowed for city buttons to return weather data after the search button is clicked

--- script.js
+++ script.js
@@ -14,14 +14,6 @@ function generateCities() {
 
 generateCities();
 
-// Checks for the name of the city on the button
-// Once complete with AJAX, make it a function. Add it to the on click event listener below afterwards
-$(".city").on("click", function () {
-    var cityName = $(this).text();
-    // Works perfectly
-    console.log(cityName);
-})
-
 // The "Go" button adds cities to the cityArray, thus growing the list on the dashboard
 $("#searchButton").on("click", function () {
     var cityList = $("#cityList");
@@ -32,12 +24,62 @@ $("#searchButton").on("click", function () {
     generateCities();
 
     // Apprently the event listener needs to be assigned again after another city is added to the list
-    $(".city").on("click", function () {
+    $(".city").on("click", function(){
         var cityName = $(this).text();
-        console.log(cityName);
-    })
-});
-
+        var apiKey = "80f8772e6cbc82b131c125fc22aa0949";
+        var mainTemp = $("#mainTemp");
+        var mainHum = $("#mainHum");
+        var windSpeed = $("#wind-speed");
+        var uvIndex = $("#uv-index");
+    
+        $.ajax({
+            url: "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey,
+            method: "GET"
+        }).then(function(response){
+            var tempF = (response.main.temp  - 273.15) * 1.80 + 32;
+            console.log(response);
+            
+            $("#cityName").text(cityName);
+            $("#mainIcon").attr("src", "http://openweathermap.org/img/w/" + response.weather[0].icon + ".png")
+            mainTemp.text("Temperature: " + tempF.toFixed(2) + " °F");
+            mainHum.text("Humidity: " + response.main.humidity +"%");
+            windSpeed.text("Wind speed: " + response.wind.speed + " MPH");
+            // Find out how to get UV
+            uvIndex.text();
+        });
+    
+        $.ajax({
+            url: "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + apiKey,
+            method: "GET"
+        }).then(function(response){
+            console.log(response);
+    
+            var tempDay1 = (response.list[0].main.temp - 273.15) * 1.80 + 32;
+            var tempDay2 = (response.list[8].main.temp - 273.15) * 1.80 + 32;
+            var tempDay3 = (response.list[16].main.temp - 273.15) * 1.80 + 32;
+            var tempDay4 = (response.list[24].main.temp - 273.15) * 1.80 + 32;
+            var tempDay5 = (response.list[32].main.temp - 273.15) * 1.80 + 32;
+    
+            $("#tempDay1").text("Temp: " + tempDay1.toFixed(2) + " °F");
+            $("#tempDay2").text("Temp: " + tempDay2.toFixed(2) + " °F");
+            $("#tempDay3").text("Temp: " + tempDay3.toFixed(2) + " °F");
+            $("#tempDay4").text("Temp: " + tempDay4.toFixed(2) + " °F");
+            $("#tempDay5").text("Temp: " + tempDay5.toFixed(2) + " °F");
+    
+            $("#humDay1").text("Humidity: " + response.list[0].main.humidity + "%");
+            $("#humDay2").text("Humidity: " + response.list[8].main.humidity + "%");
+            $("#humDay3").text("Humidity: " + response.list[16].main.humidity + "%");
+            $("#humDay4").text("Humidity: " + response.list[24].main.humidity + "%");
+            $("#humDay5").text("Humidity: " + response.list[32].main.humidity + "%");
+    
+            $("#icon1").attr("src", "http://openweathermap.org/img/w/" + response.list[0].weather[0].icon + ".png");
+            $("#icon2").attr("src", "http://openweathermap.org/img/w/" + response.list[8].weather[0].icon + ".png");
+            $("#icon3").attr("src", "http://openweathermap.org/img/w/" + response.list[16].weather[0].icon + ".png");
+            $("#icon4").attr("src", "http://openweathermap.org/img/w/" + response.list[24].weather[0].icon + ".png");
+            $("#icon5").attr("src", "http://openweathermap.org/img/w/" + response.list[32].weather[0].icon + ".png");
+    });
+    });
+})
 // This onclick event will give the weather data for that city on the dashboard
 $(".city").on("click", function(){
     var cityName = $(this).text();
@@ -55,6 +97,7 @@ $(".city").on("click", function(){
         console.log(response);
         
         $("#cityName").text(cityName);
+        $("#mainIcon").attr("src", "http://openweathermap.org/img/w/" + response.weather[0].icon + ".png")
         mainTemp.text("Temperature: " + tempF.toFixed(2) + " °F");
         mainHum.text("Humidity: " + response.main.humidity +"%");
         windSpeed.text("Wind speed: " + response.wind.speed + " MPH");
